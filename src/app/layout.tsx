3717035924ae59e73467e3d83941fd5741566112
Shivam <shivammishra15092003@gmail.com>
2024-10-07 21:34:49 +0530

    Header Section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased")}>
+      <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>
         {children}
       </body>
     </html>
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,13 @@
 export const Header = () => {
   return(
-   <div>
-    <nav>
-      <a href="#">Home</a>
-      <a href="#">Projects</a>
-      <a href="#">About</a>
-      <a href="#">Contact</a>
+   <div className="flex justify-center items-center relative top-3">
+    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <a href="#" className="nav-item">Home</a>
+      <a href="#" className="nav-item">Projects</a>
+      <a href="#" className="nav-item">About</a>
+      <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
     </nav>
-  </div> 
+  </div>  
   );
 };
  
