e337c260f168e5bd5dee968d11249ff60367cb2c
Shivam <shivammishra15092003@gmail.com>
2024-10-07 22:47:28 +0530

    Hero Section + Rings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import {Header} from "@/sections/header";
-
+import { HeroSection } from "@/sections/Hero";
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
