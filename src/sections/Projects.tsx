23153a81a030ea31a411985bb2aa776144b405c2
Shivam <shivammishra15092003@gmail.com>
2024-10-13 23:09:27 +0530

    Updated Code

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
@@ -42,5 +43,49 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+            Real-World Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4 ">
+          Hello , I am John Doe, a passionate web developer with a keen eye for
+        </p>
+        <div className="flex flex-col mt-10">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative overflow-hidden z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8 "
+            >
+              <div className="flex">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+              </div>
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5" />
+              <ul>
+                {project.results.map((result) => (
+                  <li>{result.title}</li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button>View Live Site</button>
+              </a>
+              <Image src={project.image} alt={project.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+      ;
+    </div>
+  );
 };
